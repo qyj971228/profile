1a54bc02b8023514c9c4479289d46a90f294980d
qyj <1078869122@qq.com>
2023-11-07 12:02:06 +0800

    feat: mui animation

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,24 +10,14 @@ export default function Button({ children }: { children?: ReactNode }) {
   const waveTimer = useRef<NodeJS.Timeout | null>(null)
   const [waves, setWaves] = useState<Record<string, string>[]>([])
   useEffect(() => {
-    if (waves.length > 10) {
-      setWaves([])
-    }
     waveTimer.current && clearTimeout(waveTimer.current)
     waveTimer.current = setTimeout(() => {
       setWaves([])
-    }, 3000)
+    }, 1000)
   }, [waves])
 
   // 按钮深色背景
   const [showDeepBg, setShowDeepBg] = useState<boolean>(false)
-  // 按钮样式
-  function buttonStyles(): CSSProperties {
-    if (!showDeepBg) return {}
-    return {
-      backgroundColor: '#9d9d9d',
-    }
-  }
 
   // 波纹位置
   const [waveX, setWaveX] = useState<number>(0)
@@ -38,55 +28,68 @@ export default function Button({ children }: { children?: ReactNode }) {
       height: (button.current?.clientWidth ?? 0) * 2.2,
       width: (button.current?.clientWidth ?? 0) * 2.2,
       left: waveX + 'px',
-      top: waveY + 'px'
+      top: waveY + 'px',
     }
   }
 
   const [x, y] = useMousePosition()
-
-  // 点击事件
-  function onHandleButtonClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    if (endTime - startTime > 100) return
-    setWaves([...waves, { animationName: 'scale2' }])
-    // 设置波纹位置
-    const button = e.currentTarget as HTMLElement
-    const {top, left} = button.getBoundingClientRect()
-    setWaveX(x - left)
-    setWaveY(y - top)
-    // 点击结束(弹起)恢复按钮背景颜色
-    if (!isMouseKeyDown) setShowDeepBg(false)
-  }
 
   const waveAnimationFinishedTImer = useRef<NodeJS.Timeout | null>(null)
   const [isMouseKeyDown, setIsMouseKeyDown] = useState<boolean>(false)
   const [startTime, setStartTime] = useState<number>(0)
   const [endTime, setEndTime] = useState<number>(0)
   const buttonLongPressTimer = useRef<NodeJS.Timeout | null>(null)
-  function onHandleButtonMouseDown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  // 按下
+  function onHandleButtonMouseDown(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
     setStartTime(Date.now())
     setIsMouseKeyDown(true)
     // 设置波纹位置
     const button = e.currentTarget as HTMLElement
-    const {top, left} = button.getBoundingClientRect()
+    const { top, left } = button.getBoundingClientRect()
     setWaveX(x - left)
     setWaveY(y - top)
     buttonLongPressTimer.current = setTimeout(() => {
-      setWaves([...waves, { animationName: 'scale1' }])
-    }, 100)
+      setWaves([...waves, { animationName: 'onPress' }])
+    }, 200)
     // 波纹动画完毕后修改按钮颜色为波纹颜色
     waveAnimationFinishedTImer.current = setTimeout(() => {
       setShowDeepBg(true)
-    }, 600)
+    }, 400)
   }
-  function onHandleButtonMouseUp(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  // 弹起
+  function onHandleButtonMouseUp(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
     setEndTime(Date.now())
     setShowDeepBg(false)
     setIsMouseKeyDown(false)
     buttonLongPressTimer.current && clearTimeout(buttonLongPressTimer.current)
-    // 弹起恢复按钮背景颜色
-    waveAnimationFinishedTImer.current && clearTimeout(waveAnimationFinishedTImer.current)
-    
-    // TODO: 长按中途取消
+    waveAnimationFinishedTImer.current &&
+      clearTimeout(waveAnimationFinishedTImer.current)
+  }
+  // 点击事件
+  function onHandleButtonClick(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    if (endTime - startTime > 200) return
+    setWaves([...waves, { animationName: 'click' }])
+    // 设置波纹位置
+    const button = e.currentTarget as HTMLElement
+    const { top, left } = button.getBoundingClientRect()
+    setWaveX(x - left)
+    setWaveY(y - top)
+    // 点击结束(弹起)恢复按钮背景颜色
+    if (!isMouseKeyDown) setShowDeepBg(false)
+  }
+
+  // 按钮样式
+  function buttonStyles(): CSSProperties {
+    if (!showDeepBg) return {}
+    return {
+      backgroundColor: '#9d9d9d',
+    }
   }
 
   return (
@@ -99,7 +102,11 @@ export default function Button({ children }: { children?: ReactNode }) {
       onMouseUp={(e) => onHandleButtonMouseUp(e)}
     >
       {waves.map((item, index) => (
-        <span key={index} className={style.wave} style={{...waveStyles(), animationName: item.animationName}}></span>
+        <span
+          key={index}
+          className='wave'
+          style={{ ...waveStyles(), animationName: item.animationName }}
+        ></span>
       ))}
       <label>{children ?? 'Button'}</label>
     </button>
