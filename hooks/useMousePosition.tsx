fc1335c4389087b92337c6adae78d1cde104464f
qyj <1078869122@qq.com>
2023-11-08 10:09:11 +0800

    fix: mousemove event throttle

--- hooks/useMousePosition.tsx
+++ hooks/useMousePosition.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react"
+import _ from 'lodash'
 
 export default function useMousePosition() {
   const [x, setX] = useState<number>(0)
   const [y, setY] = useState<number>(0)
   useEffect(() => {
-    document.addEventListener("mousemove", (e) => {
+    document.addEventListener("mousemove", _.throttle((e) => {
       setX(e.clientX)
       setY(e.clientY)
-    })
+    }, 100))
   }, [])
   return [x, y]
 }
