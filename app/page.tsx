8342d969d0fffd80fbb43c2f8cdf277999bd76d2
qyj <1078869122@qq.com>
2024-02-10 23:10:12 +0800

    feat: shop

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   }
 
   const toUchongdingwei = () => {
-    window.open('https://www.uchongdingwei.com', '_blank')
+    window.open('https://uchongdingwei.qiuyingjian.site', '_blank')
   }
 
   const toReactLib = () => {
@@ -41,13 +41,16 @@ export default function Home() {
     window.open('https://image-text-editor.vercel.app/', '_blank')
   }
 
-
   const toEchartMap = () => {
     window.open('https://nuxt-echart-map.vercel.app/', '_blank')
   }
 
   const toLeetCode = () => {
     window.open('https://leetcode.cn/u/qyj971228/', '_blank')
+  }
+
+  const toShop = () => {
+    window.open('https://github.com/qyj971228/shopping', '_blank')
   }
 
   const cursor = {
@@ -59,53 +62,60 @@ export default function Home() {
       <h3>This is qiuyingjian.</h3>
       <h3>Welcome to my profile.</h3>
       <ul style={{ margin: '10px' }}>
-        <Link href='/button'>本地的一些按钮</Link>
+        <Link href="/button">仿material的一些按钮</Link>
         <li
           style={{ ...cursor }}
           onClick={toUchongdingwei}
         >
-          公司项目: www.uchongdingwei.com [不可代理访问][可能服务到期]
+          公司项目: www.uchongdingwei.com[响应][网站]
         </li>
         <li style={{ ...cursor }}>
-          <span onClick={toStarBucksWeb}>仿星巴克[网站]</span>
+          <span onClick={toStarBucksWeb}>仿星巴克[响应][网站]</span>
         </li>
         <li style={{ ...cursor }}>
           <span onClick={toStarBucksCode}>仿星巴克[仓库]</span>
         </li>
-        <li
+        {/* <li
           style={{ ...cursor }}
           onClick={toLocalProject}
         >
           当前profile项目[profile.qiuyingjian.site.com][仓库]
-        </li>
+        </li> */}
         <li
           style={{ ...cursor }}
-          onClick={toEditor}>
-          图片文字编辑器
+          onClick={toEditor}
+        >
+          react图片文字编辑
         </li>
-        <li
+        {/* <li
           style={{ ...cursor }}
           onClick={toReactLib}
         >
           react组件[仓库]
-        </li>
+        </li> */}
         <li
           style={{ ...cursor }}
           onClick={toVueLib}
         >
-          vue组件[仓库]
+          vue组件[仓库][下拉框与树形组件]
         </li>
-        <li
+        {/* <li
           style={{ ...cursor }}
           onClick={toEchartMap}
         >
           echart地图大屏[studying][网站][需要代理访问]
-        </li>
-        <li
+        </li> */}
+        {/* <li
           style={{ ...cursor }}
           onClick={toSwiftUI}
         >
           swiftui[studying][仓库]
+        </li> */}
+        <li
+          style={{ ...cursor }}
+          onClick={toShop}
+        >
+          商城[施工中]
         </li>
         <li
           style={{ ...cursor }}
